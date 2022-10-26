e6b72f1468385c00aa31f2fed21a22fc648838d7
LJM <wjdals997@naver.com>
2022-10-26 14:13:01 +0900

    dss

--- src/components/MainSns.js
+++ src/components/MainSns.js
@@ -6,17 +6,21 @@ import Footer from "./Footer";
 import Login from "../components/Login.js"
 import { Route, Routes } from "react-router-dom";
 import Register from "./Register";
-import MainSns from "./MainSns";
 
-
-function App() {
-  return (
-    <Login></Login>
-    // <main>
-    // <Register></Register>
-    // </main>
-    // <MainSns></MainSns>
-  );
+function MainSns(){
+    return(
+       
+    <div className="App">
+      <Header />
+      <main>
+        <div className="container">
+          <Cards />
+          <Sidebar />
+        </div>
+      </main>
+      <Footer />
+    </div>
+        
+    );
 }
-
-export default App;
+export default MainSns;
