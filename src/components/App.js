087f9d437b580126ac1341b6823df03fc4387fd4
KDO <daonj0715@gmail.com>
2022-10-27 14:39:14 +0900

    fsdaf

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,18 @@ import { Route, Routes } from "react-router-dom";
 import Register from "../components/Account/Register.js";
 import MainSns from "./MainSns";
 import MyProfile from "./Account/MyProfile";
+import Login from "./Login";
 
 function App() {
   return (
-    <MyProfile></MyProfile>
-    // <Routes>
-    //   <Route path='/' element={<Login></Login>}></Route>
-    //   <Route path='/register' element={<Register></Register>}></Route>
-    //   <Route path='/mainsns' element={<MainSns></MainSns>}></Route>
-    // </Routes>
- 
+    <>
+      {/* <MyProfile></MyProfile> */}
+      <Routes>
+        <Route path="/" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/mainsns" element={<MainSns></MainSns>}></Route>
+      </Routes>
+    </>
   );
 }
 
